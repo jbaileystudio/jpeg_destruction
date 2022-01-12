a504020a17f05668fb3aa50b015ecab7f4616e43
jbaileystudio <james@jbaileystudio.com>
2022-01-11 20:43:37 -0500

    print and file stuff

--- jpeg_destroyer.py
+++ jpeg_destroyer.py
@@ -1,2 +1,41 @@
-for i in range(1,1000):
-	print (i)
+#print 1000 digits in a column
+#for i in range(1,10):
+#	print (i)
+
+#print("0---")
+
+# Open file and read it  #
+#f = open("/Users/ijames/Documents/GitHub/jpeg_destruction/demotext.txt", "r")
+#print(f.read())
+
+#print("1---")
+
+# Open file and specify how many characters you want read#
+#f = open("/Users/ijames/Documents/GitHub/jpeg_destruction/demotext.txt", "r")
+#print(f.read(6))
+
+#print("2---")
+
+# Open file and return first line#
+#f = open("/Users/ijames/Documents/GitHub/jpeg_destruction/demotext.txt", "r")
+#print(f.readline())
+
+#print("3---")
+
+# Open file and return first two lines#
+#f = open("/Users/ijames/Documents/GitHub/jpeg_destruction/demotext.txt", "r")
+#print(f.readline())
+#print(f.readline())
+
+#print("4---")
+
+# Loop through the file and read it line by line#
+f = open("/Users/ijames/Documents/GitHub/jpeg_destruction/demotext.txt", "r")
+for x in f:
+  print(x)
+f.close()
+
+# Close the file opened
+#f = open("/Users/ijames/Documents/GitHub/jpeg_destruction/demotext.txt", "r")
+#print(f.readline())
+#f.close()
