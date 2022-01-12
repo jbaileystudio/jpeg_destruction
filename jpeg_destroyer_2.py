c18c124047c5443225097aad8ee1fb1f1b2efe3d
jbaileystudio <james@jbaileystudio.com>
2022-01-12 15:17:56 -0500

    Update jpeg_destroyer_2.py

--- jpeg_destroyer_2.py
+++ jpeg_destroyer_2.py
@@ -12,8 +12,33 @@ with open(script_path+'/edgar.jpeg', 'rb') as image_file:
     #data = binascii.b2a_hex(image_file.read()) #no space
 
 print (data)
+print ("\n\n\n\n\n")
 #print (hex(data[100]))
 #print (data[100])
+
+middle = int(len(data)/2)
+slice_percentage_length = int(len(data)*.01)
+middle_data_slice = data[middle-int(slice_percentage_length/2):middle+int(slice_percentage_length/2)] #slice of the middle of the data
+
+
+#print (middle) # middle position in the data (bytes)
+#print (data[middle]) # decimal (representing the same value as hex a different way)
+#print (hex(data[middle])) # hex
+
+#data [middle-8:middle+8]
+print (middle_data_slice)
+
+modified_data = data
+
+starting_value = middle-int(slice_percentage_length/2)
+
+for hex_value in middle_data_slice: #remove the data from the slice
+	data.partition(starting_value)
+
+
+
+
+
 
 #hex to image
 #import binascii
